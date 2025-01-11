5ee1c6f7ff0c164bc1bc10a69e2834d4d40a7810
Ilya Rey <mezzar2468013579@gmail.com>
2025-01-12 06:06:45 +1000

    mesto-project-ff-1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content; 
+const placesList = document.querySelector('.places__list');
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function createCard (element, deleteCard) {
+    const cardPlace = cardTemplate.querySelector('.places__item').cloneNode(true);
 
+    cardPlace.querySelector('.card__image').src = element.link;
+    cardPlace.querySelector('.card__image').alt = element.alt;
+    cardPlace.querySelector('.card__title').textContent = element.name;
+
+    const deleteButton = cardPlace.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard);
+
+    return cardPlace;
+};
+
+// @todo: Функция удаления карточки
+function deleteCard(event) {
+    const buttonBin = event.target;
+    buttonBin.closest('.places__item').remove();
+};
 // @todo: Вывести карточки на страницу
+function addCard(cardPlace, placesList) {
+    placesList.append(cardPlace);
+};
+
+initialCards.forEach(function (element) {
+    addCard(createCard(element, deleteCard), placesList)
+});
